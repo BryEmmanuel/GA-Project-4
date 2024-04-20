c45b04f4a46fc80bd274a0bbe5f255e97d1aa324
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-20 15:21:55 +0800

    update server.js

--- express-postgresql/server.js
+++ express-postgresql/server.js
@@ -1,1 +1,28 @@
 require("dotenv").config();
+
+const express = require("express");
+
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000000,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5001;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}.`);
+});
