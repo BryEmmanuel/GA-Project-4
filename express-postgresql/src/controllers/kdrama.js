1d6d56a80ffd601e38892f51a538c1f82fd8d63c
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-21 13:26:46 +0800

    add get kdrama controller

---
+++ express-postgresql/src/controllers/kdrama.js
@@ -0,0 +1,12 @@
+// get all kdramas
+const getAllKdrama = async (req, res) => {
+  try {
+    const allKdrama = await pool.query("SELECT * FROM k_dramas");
+    res.json(allKdrama.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "failed to get all kdramas" });
+  }
+};
+
+module.exports = { getAllKdrama };
