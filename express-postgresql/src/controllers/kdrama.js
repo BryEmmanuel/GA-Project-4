8bfb4765b000614625e3c61aa517ccbce80764d8
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-21 13:38:22 +0800

    add addkdrama controller

--- express-postgresql/src/controllers/kdrama.js
+++ express-postgresql/src/controllers/kdrama.js
@@ -11,4 +11,19 @@ const getAllKdrama = async (req, res) => {
   }
 };
 
-module.exports = { getAllKdrama };
+// add kdramas
+const addKdrama = async (req, res) => {
+  try {
+    const { name, number_of_episodes, year_released, plot, image_url, genre } =
+      req.body;
+    await pool.query(
+      "INSERT INTO k_dramas (name, number_of_episodes, year_released, plot, image_url, genre) VALUES ($1, $2, $3, $4, $5, $6)",
+      [name, number_of_episodes, year_released, plot, image_url, genre]
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "failed to add kdrama" });
+  }
+};
+
+module.exports = { getAllKdrama, addKdrama };
