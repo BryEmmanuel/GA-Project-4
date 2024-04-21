f289007762d7854753efc1916a22f96d4db59d49
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-21 16:27:07 +0800

    create nav bar

--- react-node/src/components/Navbar.jsx
+++ react-node/src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Top Nav Bar</div>;
+  return (
+    <>
+      <header className="Navbar">
+        <nav className="navbar_container">
+          <div className="logo">logo</div>
+          <div className="searchbar_container">
+            <input className="searchbar" placeholder="Search"></input>
+          </div>
+          <div className="login">login</div>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Navbar;
