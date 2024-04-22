a3c8eb8eda2a654d15d0336be9ffa4139922a11f
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-22 14:01:40 +0800

    update homepage

--- react-node/src/pages/HomePage.jsx
+++ react-node/src/pages/HomePage.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./HomePage.css";
 import Navbar from "../components/Navbar";
 import SideNavBar from "../components/SideNavBar";
+import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
 
 const HomePage = () => {
+  // useFetch
+  const fetchData = useFetch();
+  // useContext
+  const userCtx = useContext(UserContext);
+
+  // track the state of kdrama card
+  const [kdramaCard, setKdramaCard] = useState([]);
+
+  // get all kdrama card
+  const displayKdrama = async () => {
+    try {
+      const res = await fetchData(
+        "/kdrama/getkdrama",
+        "GET",
+        undefined,
+        undefined
+      );
+      if (res.ok) {
+        const data = [...res.data];
+        setKdramaCard(data);
+      }
+    } catch (error) {
+      if (error.name !== "AbortError") {
+        console.error(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    displayKdrama();
+  });
+
   return (
     <>
       <Navbar></Navbar>
       <SideNavBar></SideNavBar>
-      <div className="homepage_container">Homepage</div>
+      <div className="homepage_container">
+        homepage
+        {kdramaCard.map((kdrama, index) => (
+          <Card></Card>
+        ))}
+      </div>
     </>
   );
 };
