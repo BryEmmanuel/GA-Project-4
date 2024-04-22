14bef6b82845daa6a147b8bc13472ed5bafa9734
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-22 14:00:33 +0800

    add useContext

--- react-node/src/App.jsx
+++ react-node/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -6,21 +6,40 @@ import RegisterPage from "./pages/RegisterPage";
 import KdramaPage from "./pages/KdramaPage";
 import Discussion from "./pages/Discussion";
 import Profile from "./pages/Profile";
-import Navbar from "./components/Navbar";
-import SideNavBar from "./components/SideNavBar";
+import useFetch from "./hooks/useFetch";
+import UserContext from "./context/user";
 
 function App() {
+  // track state of account
+  const [accessToken, setAccessToken] = useState("");
+  const [role, setRole] = useState("");
+  const [username, setUsername] = useState("");
+
+  // useFetch
+  const fetchData = useFetch();
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/main" />}></Route>
-        <Route path="main" element={<HomePage />}></Route>
-        <Route path="login" element={<LoginPage />}></Route>
-        <Route path="register" element={<RegisterPage />}></Route>
-        <Route path="main/:kdrama" element={<KdramaPage />}></Route>
-        <Route path="discussion" element={<Discussion />}></Route>
-        <Route path="profile" element={<Profile />}></Route>
-      </Routes>
+      <UserContext.Provider
+        value={{
+          accessToken,
+          setAccessToken,
+          role,
+          setRole,
+          username,
+          setUsername,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/main" />}></Route>
+          <Route path="main" element={<HomePage />}></Route>
+          <Route path="login" element={<LoginPage />}></Route>
+          <Route path="register" element={<RegisterPage />}></Route>
+          <Route path="main/:kdrama" element={<KdramaPage />}></Route>
+          <Route path="discussion" element={<Discussion />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
