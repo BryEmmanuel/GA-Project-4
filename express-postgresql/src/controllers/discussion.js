416fc38fdb8b4d4b6f952621fcd0020c96b9adf5
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-22 20:19:15 +0800

    create get discussion controller

--- express-postgresql/src/controllers/discussion.js
+++ express-postgresql/src/controllers/discussion.js
@@ -1,3 +1,14 @@
 const pool = require("../db/db");
 
 // get all discussion
+const getAllDiscussion = async (req, res) => {
+  try {
+    const allDiscussion = await pool.query("SELECT * FROM discussion");
+    res.json(allDiscussion.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "failed to get discussions" });
+  }
+};
+
+module.exports = { getAllDiscussion };
