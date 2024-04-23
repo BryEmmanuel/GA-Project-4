6ef2c5d356680dfe0c40738a1599ba114789b28f
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-23 16:12:08 +0800

    add update discussion router

--- express-postgresql/src/routers/discussion.js
+++ express-postgresql/src/routers/discussion.js
@@ -3,11 +3,13 @@ const {
   getAllDiscussion,
   addDiscussion,
   deleteDiscussion,
+  updateDiscussion,
 } = require("../controllers/discussion");
 const router = express.Router();
 
 router.get("/getdiscussion", getAllDiscussion);
 router.post("/adddiscussion", addDiscussion);
 router.delete("/deletediscussion/:id", deleteDiscussion);
+router.patch("/updatediscussion/:id", updateDiscussion);
 
 module.exports = router;
