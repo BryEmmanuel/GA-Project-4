d0116c518c670db73cc4b49645ce9f0713a731de
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-23 17:03:00 +0800

    add getCommentById router

--- express-postgresql/src/routers/comments.js
+++ express-postgresql/src/routers/comments.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { createComment, getAllComments } = require("../controllers/comments");
+const {
+  createComment,
+  getAllComments,
+  getCommentById,
+} = require("../controllers/comments");
 const router = express.Router();
 
 router.post("/new", createComment);
 router.get("/all", getAllComments);
+router.get("/:id", getCommentById);
 
 module.exports = router;
