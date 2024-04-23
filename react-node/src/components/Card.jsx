34c4fab8552321b9cde614f9f5082320b4cd11c7
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-23 20:25:03 +0800

    update card component

--- react-node/src/components/Card.jsx
+++ react-node/src/components/Card.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Card.css";
 
 const Card = (props) => {
   return (
     <div className="card">
-      <img src={props.image_url}></img>
+      <img src={props.image_url} className="card_image"></img>
+      <div className="card__content">
+        <p className="card__title">{props.name}</p>
+        <p className="card__description">{props.plot}</p>
+      </div>
     </div>
   );
 };
