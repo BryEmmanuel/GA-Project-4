cfe82776f0a829218b2c866d89e9f5ee8b4d7a01
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-23 17:38:49 +0800

    add delete comment router

--- express-postgresql/src/routers/comments.js
+++ express-postgresql/src/routers/comments.js
@@ -3,11 +3,13 @@ const {
   createComment,
   getAllComments,
   getCommentById,
+  deleteComment,
 } = require("../controllers/comments");
 const router = express.Router();
 
 router.post("/new", createComment);
 router.get("/all", getAllComments);
 router.get("/:id", getCommentById);
+router.delete("/delete/:id", deleteComment);
 
 module.exports = router;
