e5a38abf3976b643fcb0fe16cd084feadf5f746c
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-24 22:27:33 +0800

    update kdrama page

--- react-node/src/pages/KdramaPage.jsx
+++ react-node/src/pages/KdramaPage.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./KdramaPage.css";
+import useFetch from "../hooks/useFetch";
 
 const KdramaPage = () => {
-  return <div>K drama details</div>;
+  // useParams
+  const { kdrama } = useParams();
+  // useFetch
+  const fetchData = useFetch();
+
+  // states of the kdrama
+  const [name, setName] = useState("");
+  const [episodeNumber, setEpisodeNumber] = useState("");
+  const [year, setYear] = useState("");
+  const [plot, setPlot] = useState("");
+  const [image, setImage] = useState("");
+  const [genre, setGenre] = useState("");
+
+  const getKdramaById = async () => {
+    const res = await fetchData(
+      "/kdrama/getkdrama/" + kdrama,
+      "GET",
+      undefined,
+      undefined
+    );
+    if (res.ok) {
+      setName(res.data[0].name);
+      setEpisodeNumber(res.data[0].number_of_episodes);
+      setYear(res.data[0].year_released);
+      setPlot(res.data[0].plot);
+      setImage(res.data[0].image_url);
+      setGenre(res.data[0].genre_name);
+      console.log(res.data[0].image_url);
+    }
+  };
+
+  useEffect(() => {
+    getKdramaById();
+  }, []);
+
+  return (
+    <>
+      <div className="kdrama_page_container">
+        <div className="kdrama_card">
+          <div className="kdrama_trailer">TRAILER</div>
+          <img src={image} className="kdrama_image"></img>
+          <div className="kdrama_details">
+            <h2>{name}</h2>
+            <p>Episodes: {episodeNumber}</p>
+            <p>Year Released: {year}</p>
+            <p>{plot}</p>
+            <p>Genre: {genre}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default KdramaPage;
