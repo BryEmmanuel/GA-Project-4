8e2c40bbd45a5d784236d4d95f87fbf5aaf7e358
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-24 22:26:49 +0800

    edit navbar logo

--- react-node/src/components/Navbar.jsx
+++ react-node/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <header className="Navbar">
         <nav className="navbar_container">
-          <div className="logo">logo</div>
+          <Link to="/main">
+            <img
+              src="https://fontmeme.com/permalink/240424/1d655b74a01d07a0608a7ed82246054d.png"
+              className="logo"
+            />
+          </Link>
           <div className="searchbar_container">
             <input className="searchbar" placeholder="Search"></input>
           </div>
