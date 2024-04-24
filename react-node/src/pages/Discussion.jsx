6e84c17253bf18dea2e9e5df1254cb23dddb3334
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-24 23:03:39 +0800

    update discussion page

--- react-node/src/pages/Discussion.jsx
+++ react-node/src/pages/Discussion.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import useFetch from "../hooks/useFetch";
+import Post from "../components/Post";
 
 const Discussion = () => {
-  return <div>Discussion</div>;
+  // useFetch
+  const fetchData = useFetch();
+
+  // track state of discussion list
+  const [discussions, setDiscussions] = useState([]);
+
+  // get discussions
+  const getAllDiscussions = async () => {
+    try {
+      const res = await fetchData(
+        "/discussion/getdiscussion",
+        "GET",
+        undefined,
+        undefined
+      );
+      if (res.ok) {
+        const data = [...res.data];
+        setDiscussions(data);
+      }
+    } catch (error) {
+      if (error.name !== "AbortError") {
+        console.error(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getAllDiscussions();
+  }, []);
+
+  return (
+    <>
+      {discussions.map((post, index) => (
+        <Post
+          key={index}
+          id={post.id}
+          title={post.title}
+          number_of_likes={post.number_of_likes}
+          created_at={post.created_at}
+          k_drama_id={post.k_drama_id}
+          description={post.description}
+        ></Post>
+      ))}
+    </>
+  );
 };
 
 export default Discussion;
