546914355cad17682ab835f121f46c3bb61a1c36
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-26 21:10:20 +0800

    update side nav bar

--- react-node/src/components/SideNavBar.jsx
+++ react-node/src/components/SideNavBar.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
 import "./SideNavBar.css";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import { IoMdHome } from "react-icons/io";
+import { BiConversation } from "react-icons/bi";
+import { FaHeart } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
+import { BiLogOut } from "react-icons/bi";
 
 const navItems = ["Home", "Discussion", "Favourites", "Profile", "Logout"];
+const navIcons = [
+  <IoMdHome />,
+  <BiConversation />,
+  <FaHeart />,
+  <IoPerson />,
+  <BiLogOut />,
+];
 
 const SideNavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,13 +29,14 @@ const SideNavBar = () => {
               className="sidebar-2-burger"
               onClick={() => setIsOpen(!isOpen)}
             >
-              {isOpen ? "close" : "menu"}
+              {isOpen ? <IoClose /> : <IoMenu />}
             </button>
           </header>
           <nav>
-            {navItems.map((item) => (
+            {navItems.map((item, index) => (
               <button key={item} type="button">
-                <p>{item}</p>
+                <span style={{ color: "inherit" }}>{navIcons[index]}</span>
+                <h6>{item}</h6>
               </button>
             ))}
           </nav>
