38729cb373cab647ab5a772cb26bb4357c7414c4
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-26 17:01:15 +0800

    update navbar to display according to user role

--- react-node/src/components/Navbar.jsx
+++ react-node/src/components/Navbar.jsx
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import UserContext from "../context/user";
 
 const Navbar = () => {
+  // useContext
+  const userCtx = useContext(UserContext);
+  // logout
+  const handleLogout = () => {
+    localStorage.clear("refresh");
+    userCtx.setRole("");
+    userCtx.setAccessToken("");
+    userCtx.setProfilePic("");
+  };
+
   return (
     <>
-      <header className="Navbar">
-        <nav className="navbar_container">
-          <Link to="/main">
-            <img
-              src="https://fontmeme.com/permalink/240424/1d655b74a01d07a0608a7ed82246054d.png"
-              className="logo"
-            />
-          </Link>
-          <div className="searchbar_container">
-            <input className="searchbar" placeholder="Search"></input>
-          </div>
-          <div className="login">login</div>
-        </nav>
-      </header>
+      {userCtx.role === "user" ? (
+        <header className="Navbar">
+          <nav className="navbar_container">
+            <Link to="/main">
+              <img
+                src="https://fontmeme.com/permalink/240424/1d655b74a01d07a0608a7ed82246054d.png"
+                className="logo"
+              />
+            </Link>
+
+            <div className="searchbar_container">
+              <input className="searchbar" placeholder="Search"></input>
+            </div>
+            <div className="logout">
+              <Link
+                to="/login"
+                style={{ textDecoration: "none", color: "white" }}
+                onClick={() => {
+                  handleLogout();
+                }}
+              >
+                Logout
+              </Link>
+            </div>
+          </nav>
+        </header>
+      ) : (
+        <header className="Navbar">
+          <nav className="navbar_container">
+            <Link to="/main">
+              <img
+                src="https://fontmeme.com/permalink/240424/1d655b74a01d07a0608a7ed82246054d.png"
+                className="logo"
+              />
+            </Link>
+
+            <div className="login">
+              <Link
+                to="/login"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+      )}
     </>
   );
 };
