13e03565483204224998554de224e66344ee6c08
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-26 11:23:46 +0800

    update router

--- express-postgresql/src/routers/discussion.js
+++ express-postgresql/src/routers/discussion.js
@@ -12,6 +12,6 @@ router.get("/getdiscussion", getAllDiscussion);
 router.post("/adddiscussion", addDiscussion);
 router.delete("/deletediscussion/:id", deleteDiscussion);
 router.patch("/updatediscussion/:id", updateDiscussion);
-router.get("/getkdramadiscussion/:id", getKdramaDiscussion);
+router.get("/getkdramadiscussion/:name", getKdramaDiscussion);
 
 module.exports = router;
