26920be07069f659c2ee73ae6822d5538ed650aa
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-27 19:28:10 +0800

    update comments page

--- react-node/src/pages/Comments.jsx
+++ react-node/src/pages/Comments.jsx
@@ -47,6 +47,33 @@ const Comments = () => {
     }
   };
 
+  // function to convert time format
+  function timeAgo(date) {
+    const seconds = Math.floor((new Date() - date) / 1000);
+
+    const interval = Math.floor(seconds / 31536000);
+    if (interval > 1) return interval + " years ago";
+    if (interval === 1) return interval + " year ago";
+
+    const months = Math.floor(seconds / 2628000);
+    if (months > 1) return months + " months ago";
+    if (months === 1) return months + " month ago";
+
+    const days = Math.floor(seconds / 86400);
+    if (days > 1) return days + " days ago";
+    if (days === 1) return days + " day ago";
+
+    const hours = Math.floor(seconds / 3600);
+    if (hours > 1) return hours + " hours ago";
+    if (hours === 1) return hours + " hour ago";
+
+    const minutes = Math.floor(seconds / 60);
+    if (minutes > 1) return minutes + " minutes ago";
+    if (minutes === 1) return minutes + " minute ago";
+
+    return "just now";
+  }
+
   useEffect(() => {
     getDiscussionById();
     getCommentsOfDiscussion();
@@ -65,7 +92,8 @@ const Comments = () => {
             <Comment
               key={index}
               contents={comment.contents}
-              created_at={comment.created_at}
+              created_at={timeAgo(new Date(comment.created_at))}
+              username={comment.username}
             ></Comment>
           ))}
         </div>
