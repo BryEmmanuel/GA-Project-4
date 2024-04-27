b809399a5a92b149a676eb77f69bb3973cb2c716
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-27 18:09:28 +0800

    update comments page

--- react-node/src/pages/Comments.jsx
+++ react-node/src/pages/Comments.jsx
@@ -3,6 +3,7 @@ import useFetch from "../hooks/useFetch";
 import { useParams } from "react-router-dom";
 import "./Comments.css";
 import Navbar from "../components/Navbar";
+import Comment from "../components/Comment";
 
 const Comments = () => {
   // useFetch
@@ -32,9 +33,23 @@ const Comments = () => {
   };
 
   // get comments of this discussion post
+  const getCommentsOfDiscussion = async () => {
+    const res = await fetchData(
+      "/comments/discussion/" + id,
+      "PUT",
+      undefined,
+      undefined
+    );
+    if (res.ok) {
+      const data = [...res.data];
+      setComments(data);
+      console.log(data);
+    }
+  };
 
   useEffect(() => {
     getDiscussionById();
+    getCommentsOfDiscussion();
   }, []);
 
   return (
@@ -45,7 +60,15 @@ const Comments = () => {
           <h2>{postTitle}</h2>
           <h6>{postDescription}</h6>
         </div>
-        <div className="post_comments"></div>
+        <div className="post_comments">
+          {comments.map((comment, index) => (
+            <Comment
+              key={index}
+              contents={comment.contents}
+              created_at={comment.created_at}
+            ></Comment>
+          ))}
+        </div>
       </div>
     </>
   );
