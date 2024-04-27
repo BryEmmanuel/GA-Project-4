56a30deb3f9d0b0a180d90916968f37945e70daa
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-27 14:41:37 +0800

    update comments page

--- react-node/src/pages/Comments.jsx
+++ react-node/src/pages/Comments.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import "./Comments.css";
+import Navbar from "../components/Navbar";
 
 const Comments = () => {
-  return <div>Comments</div>;
+  // useFetch
+  const fetchData = useFetch();
+  // useParams
+  const { id } = useParams();
+
+  // track state of discussion post
+  const [postTitle, setPostTitle] = useState("");
+  const [postDescription, setPostDescription] = useState("");
+
+  // track state of comments
+  const [comments, setComments] = useState([]);
+
+  // get specific discussion
+  const getDiscussionById = async () => {
+    const res = await fetchData(
+      "/discussion/getspecificdiscussion/" + id,
+      "PUT",
+      undefined,
+      undefined
+    );
+    if (res.ok) {
+      setPostTitle(res.data[0].title);
+      setPostDescription(res.data[0].description);
+    }
+  };
+
+  // get comments of this discussion post
+
+  useEffect(() => {
+    getDiscussionById();
+  }, []);
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="post_container">
+        <div className="post_discussion">
+          <h2>{postTitle}</h2>
+          <h6>{postDescription}</h6>
+        </div>
+        <div className="post_comments"></div>
+      </div>
+    </>
+  );
 };
 
 export default Comments;
