0a8cbe97bc52a5d62c62a593303fd6dda72ae42e
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-29 23:49:56 +0800

    update comment component

--- react-node/src/components/Comment.jsx
+++ react-node/src/components/Comment.jsx
@@ -1,22 +1,18 @@
 import React, { useContext } from "react";
 import "./Comment.css";
-import UserContext from "../context/user";
 
 const Comment = (props) => {
-  // useContext
-  const userCtx = useContext(UserContext);
   return (
     <>
-      <div className="comments_container">
-        <div className="individual_comment_container">
-          <div className="username">{props.username}</div>
-          <div className="content">{props.contents}</div>
-          <div className="time">{props.created_at}</div>
-          {userCtx.role === "Admin" && (
-            <button className="delete_comment_button">Delete</button>
-          )}
+      {props.is_deleted === false && (
+        <div className="comments_container">
+          <div className="individual_comment_container">
+            <div className="username">{props.username}</div>
+            <div className="content">{props.contents}</div>
+            <div className="time">{props.created_at}</div>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
