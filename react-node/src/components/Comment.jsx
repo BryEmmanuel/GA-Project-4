b2d1ded0d1f69f12bafe4573127f938bf6af7282
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-29 22:10:16 +0800

    add admin view to comment component

--- react-node/src/components/Comment.jsx
+++ react-node/src/components/Comment.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Comment.css";
+import UserContext from "../context/user";
 
 const Comment = (props) => {
+  // useContext
+  const userCtx = useContext(UserContext);
   return (
     <>
       <div className="comments_container">
@@ -9,6 +12,9 @@ const Comment = (props) => {
           <div className="username">{props.username}</div>
           <div className="content">{props.contents}</div>
           <div className="time">{props.created_at}</div>
+          {userCtx.role === "Admin" && (
+            <button className="delete_comment_button">Delete</button>
+          )}
         </div>
       </div>
     </>
