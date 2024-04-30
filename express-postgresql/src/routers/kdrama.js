27df83a7ed6b8c3f4919ffe585c59a00b453d3e8
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-30 22:54:53 +0800

    protect kdrama endpoints

--- express-postgresql/src/routers/kdrama.js
+++ express-postgresql/src/routers/kdrama.js
@@ -6,12 +6,13 @@ const {
   deleteKdrama,
   getKdramaByName,
 } = require("../controllers/kdrama");
+const { authUser, authAdmin } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/getkdrama", getAllKdrama);
-router.post("/addkdrama", addKdrama);
-router.patch("/updatekdrama/:id", updateKdrama);
-router.delete("/deletekdrama/:name", deleteKdrama);
-router.get("/getkdrama/:name", getKdramaByName);
+router.post("/addkdrama", authAdmin, addKdrama);
+router.patch("/updatekdrama/:id", authAdmin, updateKdrama);
+router.delete("/deletekdrama/:name", authAdmin, deleteKdrama);
+router.get("/getkdrama/:name", authUser, getKdramaByName);
 
 module.exports = router;
