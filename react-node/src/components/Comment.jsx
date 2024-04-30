f86a53b023f6cceb8d0ba2d3d5db5b458f3b2ac6
BryEmmanuel <bryanlim1129@gmail.com>
2024-04-30 22:55:54 +0800

    add user delete button

--- react-node/src/components/Comment.jsx
+++ react-node/src/components/Comment.jsx
@@ -1,7 +1,11 @@
 import React, { useContext } from "react";
 import "./Comment.css";
+import UserContext from "../context/user";
 
 const Comment = (props) => {
+  // useContext
+  const userCtx = useContext(UserContext);
+
   return (
     <>
       {props.is_deleted === false && (
@@ -10,6 +14,9 @@ const Comment = (props) => {
             <div className="username">{props.username}</div>
             <div className="content">{props.contents}</div>
             <div className="time">{props.created_at}</div>
+            {userCtx.userId === props.user_id && (
+              <span className="user_delete_text">Delete</span>
+            )}
           </div>
         </div>
       )}
