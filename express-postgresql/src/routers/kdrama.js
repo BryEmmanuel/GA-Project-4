490f0afead2e125d6763491b4ac0164c53bb7411
BryEmmanuel <bryanlim1129@gmail.com>
2024-05-02 00:11:15 +0800

    add simple kdrama validator

--- express-postgresql/src/routers/kdrama.js
+++ express-postgresql/src/routers/kdrama.js
@@ -7,10 +7,12 @@ const {
   getKdramaByName,
 } = require("../controllers/kdrama");
 const { authUser, authAdmin } = require("../middleware/auth");
+const { validateAddKdrama } = require("../validators/kdrama");
+const { errorCheck } = require("../validators/errorCheck");
 const router = express.Router();
 
 router.get("/getkdrama", authUser, getAllKdrama);
-router.post("/addkdrama", authAdmin, addKdrama);
+router.post("/addkdrama", authAdmin, validateAddKdrama, errorCheck, addKdrama);
 router.patch("/updatekdrama/:id", authAdmin, updateKdrama);
 router.delete("/deletekdrama/:name", authAdmin, deleteKdrama);
 router.get("/getkdrama/:name", authUser, getKdramaByName);
