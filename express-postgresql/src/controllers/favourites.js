ad0996ca882742bf49a631a0ee84ddd9c35b3271
BryEmmanuel <bryanlim1129@gmail.com>
2024-05-01 12:59:25 +0800

    create new favourites controller

---
+++ express-postgresql/src/controllers/favourites.js
@@ -0,0 +1,33 @@
+const pool = require("../db/db");
+
+// add to favourites
+const addFavourites = async (req, res) => {
+  try {
+    const { userId, kdramaId } = req.body;
+    // check if favourites exist
+    const favouriteExist = await pool.query(
+      "SELECT * FROM favorites WHERE user_id = $1 AND kdrama_id = $2",
+      [userId, kdramaId]
+    );
+    if (favouriteExist.rows.length > 0) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "kdrama already favourited" });
+    }
+    // add favourite
+    await pool.query(
+      "INSERT INTO favorites (user_id, kdrama_id) VALUES ($1,$2)",
+      [userId, kdramaId]
+    );
+    res.status(200).json({ status: "success", msg: "kdrama favourited" });
+  } catch (error) {
+    console.error(error.message);
+    res
+      .status(400)
+      .json({ status: "error", msg: "failed to add to favourites" });
+  }
+};
+
+// get favourites of a user
+
+module.exports = { addFavourites };
