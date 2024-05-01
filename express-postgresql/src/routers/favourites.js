c4b32722bf4aac752a07b485e29263d6a37adcad
BryEmmanuel <bryanlim1129@gmail.com>
2024-05-01 13:57:56 +0800

    update favourites router

--- express-postgresql/src/routers/favourites.js
+++ express-postgresql/src/routers/favourites.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   addFavourites,
   getFavouritesOfUser,
+  removeFavourites,
 } = require("../controllers/favourites");
 const router = express.Router();
 
 router.post("/addfavourites", addFavourites);
 router.get("/userfavourites/:userId", getFavouritesOfUser);
+router.delete("/removefavourites", removeFavourites);
 
 module.exports = router;
