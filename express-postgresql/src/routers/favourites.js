f1ec8fd8a0b3e34b5fd9677cd13e00bc5be5daca
BryEmmanuel <bryanlim1129@gmail.com>
2024-05-01 13:18:46 +0800

    add new favourites route

--- express-postgresql/src/routers/favourites.js
+++ express-postgresql/src/routers/favourites.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { addFavourites } = require("../controllers/favourites");
+const {
+  addFavourites,
+  getFavouritesOfUser,
+} = require("../controllers/favourites");
 const router = express.Router();
 
 router.post("/addfavourites", addFavourites);
+router.get("/userfavourites/:userId", getFavouritesOfUser);
 
 module.exports = router;
