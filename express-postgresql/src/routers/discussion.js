d783a15a597d1083ab21a1f0a0191c7fede25a4f
BryEmmanuel <bryanlim1129@gmail.com>
2024-05-02 00:11:25 +0800

    add simple discussion validator

--- express-postgresql/src/routers/discussion.js
+++ express-postgresql/src/routers/discussion.js
@@ -10,13 +10,30 @@ const {
   decreaseLikes,
 } = require("../controllers/discussion");
 const { authUser, authAdmin } = require("../middleware/auth");
+const {
+  validateAddDiscussion,
+  validateNameInParam,
+} = require("../validators/discussion");
+const { errorCheck } = require("../validators/errorCheck");
 const router = express.Router();
 
 router.get("/getdiscussion", authUser, getAllDiscussion);
-router.post("/adddiscussion", authUser, addDiscussion);
+router.post(
+  "/adddiscussion",
+  authUser,
+  validateAddDiscussion,
+  errorCheck,
+  addDiscussion
+);
 router.delete("/deletediscussion/:id", authAdmin, deleteDiscussion);
 router.patch("/updatediscussion/:id", authAdmin, updateDiscussion);
-router.get("/getkdramadiscussion/:name", authUser, getKdramaDiscussion);
+router.get(
+  "/getkdramadiscussion/:name",
+  authUser,
+  validateNameInParam,
+  errorCheck,
+  getKdramaDiscussion
+);
 router.put("/getspecificdiscussion/:id", authUser, getSpecificDiscussion);
 router.patch("/increaselikes/:id", authUser, increaseLikes);
 router.patch("/decreaselikes/:id", authUser, decreaseLikes);
